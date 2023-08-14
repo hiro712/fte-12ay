a0f96d8d5a9bd7290503b6563e575f318d55ac48
Hiroto Abe <abe15263076@gmail.com>
2023-08-14 23:49:29 +0900

    add firebase

---
+++ src/contexts/useAuth.ts
@@ -0,0 +1,37 @@
+import { app } from '@/configs/firebase/firebase';
+import {
+  GoogleAuthProvider,
+  User,
+  getAuth,
+  onAuthStateChanged,
+  signInWithRedirect,
+  signOut,
+} from 'firebase/auth';
+import { useEffect, useState } from 'react';
+
+export const useAuth = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const auth = getAuth(app);
+    return onAuthStateChanged(auth, (user) => {
+      console.log('hook user: ', user);
+      setUser(user);
+      setIsLoading(false);
+    });
+  }, [setUser]);
+
+  const login = (): Promise<void> => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth(app);
+    return signInWithRedirect(auth, provider);
+  };
+
+  const logout = (): Promise<void> => {
+    const auth = getAuth(app);
+    return signOut(auth);
+  };
+
+  return { isLoading, user, login, logout };
+};
